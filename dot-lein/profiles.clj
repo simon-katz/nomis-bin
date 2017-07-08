d9d5ba1537e5b472220de508c9691d482d0d9dc9
Simon Katz <sk@nomistech.com>
2017-07-08 13:44:05 +0100

    lein profiles.clj: Add [lein-nomis-ns-graph "0.3.0"] and comment out loads of stuff and update some things

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -6,39 +6,40 @@
     '{:user
       {:test-paths   ["_nomis-non-vcs-stuff/test"]
        :dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"]
-                      [walmartlabs/system-viz "0.1.1"]
+                      ;; [walmartlabs/system-viz "0.1.1"]
                       ;; ;; [slamhound "1.3.3"]
-                      [spyscope "0.1.5"]
+                      [spyscope "0.1.6"]
                       ;; [criterium "0.4.2"]
-                      [org.clojure/java.classpath "0.2.0"]
+                      [org.clojure/java.classpath "0.2.3"]
 
-                      [pjstadig/humane-test-output "0.7.0"]
+                      ;; [pjstadig/humane-test-output "0.7.0"]
                       
                       ;; #### Look at these again
-                      [com.cemerick/pomegranate "0.3.1"]
+                      ;; [com.cemerick/pomegranate "0.3.1"]
                       ;; [com.nomistech/emacs-hacks-in-clojure "0.1.0-SNAPSHOT"]
                       ;; [com.nomistech/clojure-dev-utils "0.1.0-SNAPSHOT"]
                       ]
-       :plugins [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]
-                 [lein-nomis-ns-graph "0.2.0-SNAPSHOT"]
-                 [com.jakemccrary/lein-test-refresh "0.9.0"]
-                 [lein-auto "0.1.2"]
+       :plugins [[lein-nomis-ns-graph "0.3.0"]
+                 ;; [com.jakemccrary/lein-test-refresh "0.9.0"]
+                 ;; [lein-auto "0.1.2"]
                  
                  ;; ;; TODO: Remind yourself of these...
                  ;; [lein-pprint "1.1.1"]
-                 [lein-kibit "0.1.2"]
-                 [jonase/eastwood "0.2.3"]
+                 ;; [lein-kibit "0.1.2"]
+                 ;; [jonase/eastwood "0.2.3"]
                  
-                 [lein-ancient "0.6.10"]
-                 [lein-try "0.4.3"]]
+                 ;; [lein-ancient "0.6.10"]
+                 ;; [lein-try "0.4.3"]
+                 ]
        :aliases {
                  ;; "slamhound" ["run" "-m" "slam.hound"]
                  }
        :test-refresh {:quiet true}
        :global-vars {
                      ;; *warn-on-reflection* true
-                     *print-level*        30
-                     *print-length*       1000}
+                     ;; *print-level*        30
+                     ;; *print-length*       1000
+                     }
        :injections
        [(try
           ;; try/catch because "lein repl" outside a project dir
@@ -46,15 +47,16 @@
           (require 'spyscope.core)
           (catch RuntimeException e))
 
-        (require 'pjstadig.humane-test-output)
-        (pjstadig.humane-test-output/activate!)
+        ;; (require 'pjstadig.humane-test-output)
+        ;; (pjstadig.humane-test-output/activate!)
 
-        (require 'com.walmartlabs.system-viz)
-        (defn viz
-          ([sys]
-           (viz sys {}))
-          ([sys opts]
-           (com.walmartlabs.system-viz/visualize-system sys opts)))]}}
+        ;; (require 'com.walmartlabs.system-viz)
+        ;; (defn viz
+        ;;   ([sys]
+        ;;    (viz sys {}))
+        ;;   ([sys opts]
+        ;;    (com.walmartlabs.system-viz/visualize-system sys opts)))
+        ]}}
     
     (let [local-extras-file (str (System/getProperty "user.home")
                                  "/.lein/profiles-local-extras.clj")]
