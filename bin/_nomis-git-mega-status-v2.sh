307267e82750fb9e522ccdd5e410607e712ebc76
Simon Katz <sk@nomistech.com>
2017-08-14 20:35:00 +0100

    Filter out more stuff in `_nomis-git-mega-status-v2.sh`

--- bin/_nomis-git-mega-status-v2.sh
+++ bin/_nomis-git-mega-status-v2.sh
@@ -1,4 +1,9 @@
 # Run this by passing it to bash as a command line arg.
 # This shebang doesn't work on Android: #!/bin/bash
 
-_nomis-git-mega-status-v1.sh | grep -v "^## master...origin/master$"
+_nomis-git-mega-status-v1.sh | \
+    grep -v "^## master...origin/master$" | \
+    grep -v "^## tutorial-18...origin/tutorial-18$" | \
+    grep -v "^## nomis...origin/nomis$" | \
+    grep -v "^## nomis-0-10-0...origin/nomis-0-10-0$" | \
+    grep -v "^## simon-post-dojo-stuff...origin/simon-post-dojo-stuff$" 
