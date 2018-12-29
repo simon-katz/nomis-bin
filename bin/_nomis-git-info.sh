6c9ac92453d71c2d789c2840f447a1753a18e832
Simon Katz <sk@nomistech.com>
2018-12-29 16:04:54 +0000

    Change "_nomis-git-info.sh" to give more info

--- bin/_nomis-git-info.sh
+++ bin/_nomis-git-info.sh
@@ -1,5 +1,11 @@
 # Run this by passing it to bash as a command line arg.
 # This shebang doesn't work on Android: #!/bin/bash
 
-git status -s -b
+# Short status of current branch.
+git status --short --branch
+
+# Branches which are ahead or behind.
 git branch -vv | grep ahead\\\|behind
+
+# Local branches with no remote.
+git branch -vv | cut -c 3- | awk '$3 !~/\[/ { print "Only local: "$1 }'
