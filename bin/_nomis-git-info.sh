4710d8ff1838a804f048ab987fd288c001f03879
Simon Katz <sk@nomistech.com>
2018-12-29 15:52:05 +0000

    Introduce "_nomis-git-info.sh.sh"
    This allows you to do multiple things from "_nomis-git-mega-status-v1.sh".

--- bin/_nomis-git-info.sh
+++ bin/_nomis-git-info.sh
@@ -1,4 +1,5 @@
 # Run this by passing it to bash as a command line arg.
 # This shebang doesn't work on Android: #!/bin/bash
 
-_nomis-do-to-all-git-repos.sh _nomis-git-info.sh
+git status -s -b
+git branch -vv | grep ahead\\\|behind
